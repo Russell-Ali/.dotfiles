66d19e63deab3bf951a5d3acbbc57f02a4e5a886
rasul-ali <resul.eliyev2000@mail.ru>
2022-08-25 22:46:57 +0400

    cmp stuff

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -28,18 +28,12 @@ local kind_icons = {
     Operator = "",
     TypeParameter = ""
 }
+
 cmp.setup({
     formatting = {
-        format = function(entry, vim_item)
+        format = function(_, vim_item)
             -- Kind icons
-            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
-            -- Source
-            vim_item.menu = ({
-                buffer = "[BUF]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[SNP]",
-                nvim_lua = "[Lua]",
-            })[entry.source.name]
+            vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
             return vim_item
         end
     },
