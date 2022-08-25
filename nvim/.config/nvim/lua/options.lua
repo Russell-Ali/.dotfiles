24a21bfd2b639a4cfdbc5d77765d6c476b0cf452
rasul-ali <resul.eliyev2000@mail.ru>
2022-08-25 22:52:05 +0400

    empty

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,50 +3,50 @@ local HOME = os.getenv('HOME')
 
 -- List of options
 local options = {
-number = true,
-relativenumber = true,
-mouse = 'nv',
-guicursor = '',
-colorcolumn = '100',
-tabstop = 4,
-shiftwidth = 4,
-expandtab = true,
-errorbells = false,
-swapfile = false,
-undodir = HOME .. '/.local/share/nvim/undo',
-undofile = true,
-wrap = false,
-scrolloff = 8,
-smartindent = true,
-hlsearch = false,
-showmode = false,
-signcolumn = 'yes',
-laststatus = 3,
-ignorecase = true,
-smartcase = true,
-filetype = 'on',
-background = 'dark',
-spelllang = 'en_us',
-spellsuggest = { 'best' , 9},
-updatetime = 400,
-termguicolors = true,
-completeopt = { 'menu' , 'menuone' , 'noselect' },
-cursorline = true,
-splitright = true,
-splitbelow = true,
-syntax = 'ON',
+    number = true,
+    relativenumber = true,
+    mouse = 'nv',
+    guicursor = '',
+    colorcolumn = '100',
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    errorbells = false,
+    swapfile = false,
+    undodir = HOME .. '/.local/share/nvim/undo',
+    undofile = true,
+    wrap = false,
+    scrolloff = 8,
+    smartindent = true,
+    hlsearch = false,
+    showmode = false,
+    signcolumn = 'yes',
+    laststatus = 3,
+    ignorecase = true,
+    smartcase = true,
+    filetype = 'on',
+    background = 'light',
+    spelllang = 'en_us',
+    spellsuggest = { 'best', 9 },
+    updatetime = 400,
+    termguicolors = true,
+    completeopt = { 'menu', 'menuone', 'noselect' },
+    cursorline = true,
+    splitright = true,
+    splitbelow = true,
+    syntax = 'ON',
 }
 -- Function to set from table above
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 -- List of options to append to the defaults
 local appends = {
-complete = 'kspell',
-fillchars = [[eob: ]]
+    complete = 'kspell',
+    fillchars = [[eob: ]]
 }
 -- Function to append from table above
 for k, v in pairs(appends) do
-  vim.opt[k]:append(v)
+    vim.opt[k]:append(v)
 end
